2176d5b0d8ec096aa749063500052c9f6f6b5be7
Niko Muhamad Fajar <90367273+nameisjar@users.noreply.github.com>
2025-09-19 17:36:13 +0700

    add feature timer

--- second_win.py
+++ second_win.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTime, QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from PyQt5.QtGui import QFont
 from final_win import FinalWin
 
 class TestWin(QWidget):
@@ -20,8 +21,9 @@ class TestWin(QWidget):
         self.lbl_first = QLabel('tahap pertama')
         self.lbl_second = QLabel('tahap kedua')
         self.lbl_three = QLabel('tahap ketiga')
-        self.stopwatch = QLabel('09:00:09')
-        
+        self.stopwatch = QLabel('00:00:00')
+        self.stopwatch.setFont(QFont('Times', 36, QFont.Bold))
+
         self.btn_first = QPushButton('start the first test')
         self.btn_second = QPushButton('start the doing squats')
         self.btn_three = QPushButton('start the final test')
@@ -57,13 +59,68 @@ class TestWin(QWidget):
         self.h_line.addLayout(self.r_line)
         
         self.setLayout(self.h_line)
-        
-        
     
     def next_win(self):
         self.fw = FinalWin()
         self.hide()
-        
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.stopwatch.setText(time.toString("hh:mm:ss"))
+        self.stopwatch.setFont(QFont("Times", 36, QFont.Bold))
+        self.stopwatch.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString('hh:mm:ss') == '00:00:00':
+            self.timer.stop()
+
+    def timer_test(self):
+        global time
+        time = QTime(0, 0, 15)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.stopwatch.setText(time.toString("hh:mm:ss")[6:8])
+        self.stopwatch.setStyleSheet("color: rgb(0,0,0)")
+        self.stopwatch.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    
+    def timer_sits(self):
+        global time
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.stopwatch.setText(time.toString("hh:mm:ss"))
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.stopwatch.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.stopwatch.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.stopwatch.setStyleSheet("color: rgb(0,0,0)")
+        self.stopwatch.setFont(QFont("Times", 36, QFont.Bold))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer_final(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
     
     def connects(self):
         self.btn_result.clicked.connect(self.next_win)
+        self.btn_first.clicked.connect(self.timer_test)
+        self.btn_second.clicked.connect(self.timer_sits)
+        self.btn_three.clicked.connect(self.timer_final)
+        
+        
